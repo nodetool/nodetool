f82d5652f8d315212e27a3142a8f9bf82c974cad
Blusk <bluskript@gmail.com>
2022-08-07 01:17:38 -0400

    add command to query nodes (but it doesn't reflect what the backend actually has)

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,10 +1,27 @@
 #![cfg_attr(
-  all(not(debug_assertions), target_os = "windows"),
-  windows_subsystem = "windows"
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
 )]
 
+use std::sync::Arc;
+
+use nodetool_sdk::{node::NodeDescriptor, node_graph::NodeGraph};
+use parking_lot::Mutex;
+
+struct NodetoolState(NodeGraph);
+
+#[tauri::command]
+fn query_nodes(state: tauri::State<Arc<Mutex<NodetoolState>>>) -> Vec<NodeDescriptor> {
+    vec![NodeDescriptor {
+        name: "add".to_string(),
+        categories: vec!["math".to_string()],
+    }]
+}
+
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .manage(Arc::new(Mutex::new(NodetoolState(NodeGraph::new()))))
+        .invoke_handler(tauri::generate_handler![query_nodes])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
